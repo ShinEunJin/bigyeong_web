506fcdd1aa22f60e2a3bd151f1d8f763fe750b55
ShinEunJin <sineun5501@naver.com>
2022-09-15 00:02:02 +0900

    FirstSection 마우스 아이콘 추가

--- src/views/Home/FirstSection.tsx
+++ src/views/Home/FirstSection.tsx
@@ -1,19 +1,47 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 const Container = styled.section`
   height: 100vh;
   width: 100%;
   text-align: center;
   padding-top: 30vh;
+  position: relative;
 `;
 
 const MainText = styled.div`
-  font-size: 3.5rem;
+  font-size: 3.5em;
   font-weight: bolder;
-  line-height: 5rem;
+  line-height: 1.5em;
 `;
 
-const FirstSection = () => {
+const DragAnimation = keyframes`
+  0% {
+    opacity: 0.15;
+    transform: translateY(0px);
+  }
+  50% {
+    opacity: 0.25;
+    transform: translateY(20px);
+  }
+  100% {
+    opacity: 0.15;
+    transform: translateY(0px);
+  }
+`;
+
+const DragImg = styled.img`
+  width: 3em;
+  height: 3em;
+  position: absolute;
+  bottom: 6em;
+  animation: ${DragAnimation} 2s infinite;
+`;
+
+interface FirstSectionProps {
+  active: boolean;
+}
+
+const FirstSection = (props: FirstSectionProps) => {
   return (
     <Container>
       <MainText>
@@ -21,6 +49,9 @@ const FirstSection = () => {
         <br />
         With My Bike
       </MainText>
+      {props.active && (
+        <DragImg src={require("@/assets/images/home/drag.png")} />
+      )}
     </Container>
   );
 };
